4f7228ffa932fde3019f67bcff212a7400fc3b76
Leonardo Fonseca Pontes <Pontes@RAOCSCCNX215.coc.com.br>
2019-09-18 11:28:45 -0300

    primeiro commit do projeto

---
+++ src/PkgMain/Main.java
@@ -0,0 +1,9 @@
+package PkgMain;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Olá Mundo!");
+	};
+}
---
+++ src/module-info.java
@@ -0,0 +1,4 @@
+module TBAA {
+	requires java.desktop;
+
+}
